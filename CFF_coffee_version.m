c37ce8549f1ed47b479a23e984bac33f1f9146ed
Dr. Alexandre C. G. Schimel <alex.schimel@protonmail.com>
2023-10-19 09:54:54 +0200

    New tag

--- CFF_coffee_version.m
+++ CFF_coffee_version.m
@@ -22,7 +22,8 @@ function ver = CFF_coffee_version()
 %   Authors: Alex Schimel (NGU, alexandre.schimel@ngu.no) and Yoann Ladroit
 %   (Kongsberg Maritime, yoann.ladroit@km.kongsberg.com) 
 
-ver = '2.0.0-alpha.11'; % 27/07/2023
+ver = '2.0.0-alpha.12'; % 19/10/2023
+% ver = '2.0.0-alpha.11'; % 27/07/2023
 % ver = '2.0.0-alpha.10'; % 26/07/2023
 % ver = '2.0.0-alpha.9'; % 14/07/2023
 % ver = '2.0.0-alpha.8'; % 07/07/2023
