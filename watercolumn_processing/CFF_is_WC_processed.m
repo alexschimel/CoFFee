1ad2605f5ec44a9a34c3edea1fce132b48484701
Dr. Alexandre C. G. Schimel <alex.schimel@protonmail.com>
2024-12-12 19:14:25 +0100

    support for cell array of fData

--- watercolumn_processing/CFF_is_WC_processed.m
+++ watercolumn_processing/CFF_is_WC_processed.m
@@ -1,7 +1,13 @@
-function bool = CFF_is_WC_processed(fData)
+function bool = CFF_is_WC_processed(fDataInput)
 %CFF_IS_WC_PROCESSED  Check if fData has processed WCD
 %
-%   BOOL = CFF_PROCESS_WC(FDATA) returns True if FDATA has processed WCD.
+%   BOOL = CFF_PROCESS_WC(FDATA) where FDATA is a single fData structure
+%   returns True if FDATA has processed WCD, otherwise False.
+
+%   BOOL = CFF_PROCESS_WC(FDATAGROUP) where FDATAGROUP is a cell array of
+%   fData structures returns a vector of booleans where each element is
+%   True or False whether the corresponding fData element of FDATAGROUP has
+%   processed WCD. 
 %
 %   See also CFF_IS_FDATA_VERSION_CURRENT
 
@@ -10,10 +16,15 @@ function bool = CFF_is_WC_processed(fData)
 
 % Input arguments management
 p = inputParser;
-addRequired(p,'fData',@(x) CFF_is_fData_version_current(x)); % source fData
-parse(p,fData);
-fData = p.Results.fData;
+addRequired(p,'fData',@(x) all(CFF_is_fData_version_current(x)));
+parse(p,fDataInput);
+fDataInput = p.Results.fData;
 clear p
 
 % condition
-bool = isfield(fData,'X_SBP_WaterColumnProcessed');
+testFun = @(x) isfield(x,'X_SBP_WaterColumnProcessed');
+if isstruct(fDataInput)
+    bool = testFun(fDataInput);
+else
+    bool = cellfun(testFun,fDataInput);
+end
